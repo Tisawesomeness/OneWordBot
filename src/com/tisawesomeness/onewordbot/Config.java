0c4bcc260dd8092c47e0b8fc14db63654a09d84b
Tisawesomeness <creeperscreep.tk@gmail.com>
2017-02-07 17:21:19 -0500

    Testing

--- src/com/tisawesomeness/onewordbot/Config.java
+++ src/com/tisawesomeness/onewordbot/Config.java
@@ -1,10 +1,8 @@
 package com.tisawesomeness.onewordbot;
 
 import java.io.File;
-import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Config {
@@ -40,8 +38,8 @@ public class Config {
 			doubleMessage = lang.optString("doubleMessage");
 			invalidWord = lang.optString("invalidWord");
 			
-		} catch (JSONException | IOException e) {
-			e.printStackTrace();
+		} catch (Exception ex) {
+			ex.printStackTrace();
 		}
 	}
 
